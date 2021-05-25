ec34517afb98870341c3a245944a680627d3b9d4
ediprekpalaj <79288178+ediprekpalaj@users.noreply.github.com>
2021-05-25 13:13:18 -0400

    Add files via upload

---
+++ q912.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
