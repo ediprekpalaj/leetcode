faabd4879619ce84e748d3c15f228cb0ab670d5d
Edi <79288178+ediprekpalaj@users.noreply.github.com>
2021-06-06 22:38:28 -0400

    Update q26.py

--- q26.py
+++ q26.py
@@ -1,13 +1,10 @@
 class Solution:
         def removeDuplicates(self, nums: List[int]) -> int:
-        i = 0
-        j = 1
-        leng = len(nums) #3
-        while(j < leng):  #1 < 3
-            if nums[i] == nums[j]:  
-                nums.pop(j)
-            else:
-                i+=1
-                j+=1
-            leng = len(nums) 
-        return len(nums)
+            i = 0
+            while(i < len(nums) - 1):
+                j = i + 1
+                if nums[i] == nums[j]:  
+                    del nums[j]
+                else:
+                    i+=1
+            return len(nums)
